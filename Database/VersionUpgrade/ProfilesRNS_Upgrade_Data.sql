7df7e79eca4e8a4b59750de279573c8b9713eff3
Nick Brown <nbrown@recomdata.com>
2013-08-08 15:05:15 -0400

    Adding UMASS to DIRECT

--- Database/VersionUpgrade/ProfilesRNS_Upgrade_Data.sql
+++ Database/VersionUpgrade/ProfilesRNS_Upgrade_Data.sql
@@ -2,22 +2,82 @@
 
 Run this script on:
 
-	Profiles RNS Version 1.0.3
+	Profiles RNS Version 1.0.4
 
 to update its data to:
 
-	Profiles RNS Version 1.0.4
+	Profiles RNS Version 2.0.0
 
 *** You are recommended to back up your database before running this script!
+*** This script will delete any customizations made to [Ontology.Presentation].[XML]
+*** If you have customized the presentation XML you will need to manually merge these changes.
 
 *** Make sure you run the ProfilesRNS_Upgrade_Schema.sql file before running this file.
 
+*** Modify line 21 so that it points to the location of the InstallData.xml file that came with Profiles RNS 2.0.0 (not an older version of that file). The file has to be on the database server, not your local machine.
+
 */
 
-update [ontology.].classproperty
-       set editsecuritygroup=-20, EditPermissionsSecurityGroup=-20
-       where Class='http://xmlns.com/foaf/0.1/Person' and NetworkProperty is null and Property='http://vivoweb.org/ontology/core#mailingAddress'
+EXEC [Framework.].[LoadXMLFile] @FilePath = 'c:\ProfilesRNS\Database\Data\InstallData.xml', @TableDestination = '[Framework.].[InstallData]', @DestinationColumn = 'DATA'
 
-update [ontology.].classproperty
-       set editsecuritygroup=-20, EditPermissionsSecurityGroup=-20
-       where Class='http://xmlns.com/foaf/0.1/Person' and NetworkProperty is null and Property='http://profiles.catalyst.harvard.edu/ontology/prns#emailEncrypted'
+ DECLARE @x XML
+ SELECT @x = ( SELECT TOP 1
+                        Data
+               FROM     [Framework.].[InstallData]
+               ORDER BY InstallDataID DESC
+             ) 
+
+truncate table [Ontology.Presentation].[XML]			 
+			 
+ --[Ontology.Presentation].[XML]
+ INSERT INTO [Ontology.Presentation].[XML]
+         ( PresentationID,
+			type ,
+           subject ,
+           predicate ,
+           object ,
+           presentationXML ,
+           _SubjectNode ,
+           _PredicateNode ,
+           _ObjectNode
+         )       
+  SELECT  R.x.value('PresentationID[1]', 'varchar(max)') ,
+		  R.x.value('type[1]', 'varchar(max)') ,
+          R.x.value('subject[1]', 'varchar(max)'),
+          R.x.value('predicate[1]', 'varchar(max)'),
+          R.x.value('object[1]', 'varchar(max)'),
+          (case when CAST(R.x.query('presentationXML[1]/*') AS NVARCHAR(MAX))<>'' then R.x.query('presentationXML[1]/*') else NULL end) , 
+          R.x.value('_SubjectNode[1]', 'varchar(max)'),
+          R.x.value('_PredicateNode[1]', 'varchar(max)'),
+          R.x.value('_ObjectNode[1]', 'varchar(max)')
+  FROM    ( SELECT
+                      @x.query
+                      ('Import[1]/Table[@Name=''[Ontology.Presentation].[XML]'']')
+                      x
+          ) t
+  CROSS APPLY x.nodes('//Row') AS R ( x )
+  
+  
+  truncate table [Direct.].[Sites]
+    --[Direct.].[Sites]
+  INSERT INTO [Direct.].[Sites]
+          ( SiteID ,
+            BootstrapURL ,
+            SiteName ,
+            QueryURL ,
+            SortOrder ,
+            IsActive
+          )
+  SELECT	R.x.value('SiteID[1]','varchar(max)'),
+			R.x.value('BootstrapURL[1]','varchar(max)'),
+			R.x.value('SiteName[1]','varchar(max)'),
+			R.x.value('QueryURL[1]','varchar(max)'),
+			R.x.value('SortOrder[1]','varchar(max)'),
+			R.x.value('IsActive[1]','varchar(max)')
+	 FROM    ( SELECT
+                      @x.query
+                      ('Import[1]/Table[@Name=''[Direct.].[Sites]'']')
+                      x
+          ) t
+  CROSS APPLY x.nodes('//Row') AS R ( x )
+	
