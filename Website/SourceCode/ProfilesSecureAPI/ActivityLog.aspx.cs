38113ccbc1ef0cf007532133f810ed5cac062be5
Eric Meeks <eric.meeks@ucsf.edu>
2013-08-30 13:11:04 -0700

    Added web api to get activity log info

---
+++ Website/SourceCode/ProfilesSecureAPI/ActivityLog.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ProfilesSecureAPI
+{
+    public partial class ActivityLog : System.Web.UI.Page
+    {
+        private static string profilesdb = ConfigurationManager.ConnectionStrings["ProfilesDB"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string methodName = Request["MethodName"];
+            string afterDate = Request["AfterDate"];
+
+            Response.ContentType = "text/plain";
+            using (SqlConnection conn = new SqlConnection(profilesdb)) 
+            {
+                conn.Open();
+                SqlCommand dbcommand = new SqlCommand("[UCSF].[ReadActivityLog]", conn);
+                dbcommand.CommandType = CommandType.StoredProcedure;
+                dbcommand.Parameters.Add(methodName != null ? new SqlParameter("@methodName", methodName) : new SqlParameter("@methodName", DBNull.Value));
+                dbcommand.Parameters.Add(afterDate != null ? new SqlParameter("@afterDT", afterDate) : new SqlParameter("@afterDT", DBNull.Value));
+                using (SqlDataReader dbreader = dbcommand.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (dbreader.Read())
+                    {
+                        Response.Write(dbreader[0].ToString() + ", '" +
+                                       dbreader[1].ToString() + "', " +
+                                       dbreader[2].ToString() + ", " +
+                                       dbreader[3].ToString() + ", " +
+                                       dbreader[4].ToString() + Environment.NewLine);
+                    }
+                }
+            }
+        }
+    }
+}
