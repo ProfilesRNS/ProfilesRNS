93edc24d34e19f9e70f50ec34497c1ca92731fd4
Nick Brown <nicholas_brown@hms.harvard.edu>
2014-06-16 16:08:14 -0400

    Formatting fix for gadgets.

--- Website/SourceCode/Profiles/Profiles/ORNG/Modules/ViewPersonalGadget/ViewPersonalGadget.ascx.cs
+++ Website/SourceCode/Profiles/Profiles/ORNG/Modules/ViewPersonalGadget/ViewPersonalGadget.ascx.cs
@@ -64,7 +64,7 @@ namespace Profiles.ORNG.Modules.Gadgets
             // UCSF OpenSocial items
             if (gadget != null && om.IsVisible())
             {
-                litGadget.Text = "<div id='" + gadget.GetChromeId() + "' class='gadgets-gadget-parent'></div>";
+                litGadget.Text = "<table><tr><td><div id='" + gadget.GetChromeId() + "' class='gadgets-gadget-parent'></div></td></tr></table>";
                 om.LoadAssets();
             }
         }
