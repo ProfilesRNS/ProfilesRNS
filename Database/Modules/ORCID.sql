8690080c07e4073ca6fcffa79236ff828cd30052
Nick Brown <nicholas_brown@hms.harvard.edu>
2014-07-31 15:19:23 -0400

    Setting up ORCID as an optional module

---
+++ Database/Modules/ORCID.sql
@@ -0,0 +1,12 @@
+update [Ontology.].ClassProperty
+	set IsDetail = 1,
+		CustomDisplay = 1,
+		CustomEdit = 1,
+		EditSecurityGroup = -20,
+		EditPermissionsSecurityGroup = -20,
+		EditExistingSecurityGroup = -20,
+		EditAddNewSecurityGroup = -20,
+		MaxCardinality = 1,
+		CustomEditModule = '<Module ID="CustomEditORCID" />'
+	where class = 'http://xmlns.com/foaf/0.1/Person'
+		and property = 'http://vivoweb.org/ontology/core#orcidId'
