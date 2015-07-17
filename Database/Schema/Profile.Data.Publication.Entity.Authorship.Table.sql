9c9bca6491d90ecd024d5218356de21bd5b16d20
unknown <nwb4@NICKNICK-PC.MED.HARVARD.EDU>
2015-07-17 11:20:17 -0400

    Remove duplicate indexes. Closes #52

--- Database/Schema/Profile.Data.Publication.Entity.Authorship.Table.sql
+++ Database/Schema/Profile.Data.Publication.Entity.Authorship.Table.sql
@@ -48,18 +48,3 @@ INCLUDE ( [EntityID],
 [YearWeight],
 [InformationResourceID]) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 GO
-CREATE NONCLUSTERED INDEX [missing_index_73175] ON [Profile.Data].[Publication.Entity.Authorship] 
-(
-	[IsActive] ASC
-)
-INCLUDE ( [EntityID],
-[EntityName],
-[EntityDate],
-[authorPosition],
-[authorRank],
-[PersonID],
-[numberOfAuthors],
-[authorWeight],
-[YearWeight],
-[InformationResourceID]) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-GO
