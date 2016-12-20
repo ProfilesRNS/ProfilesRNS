9f6d91d65ca2d5ee8f8d8f9bc1ebd5c4b541c0cd
unknown <nwb4@NICKNICK-PC.MED.HARVARD.EDU>
2016-12-20 11:05:11 -0500

    Typo in datamap install data, and property missing from [Ontology.].[ClassProperty] table #124

--- Database/VersionUpgrade_2.9.0_2.10.0/ProfilesRNS_Upgrade_Data.sql
+++ Database/VersionUpgrade_2.9.0_2.10.0/ProfilesRNS_Upgrade_Data.sql
@@ -42,6 +42,17 @@ DELETE FROM [Ontology.].[DataMap] WHERE Class = 'http://vivoweb.org/ontology/cor
 -- Update Datamap to add Role type to ResearcherRole nodes
 UPDATE [Ontology.].[DataMap] SET MapTable = '[Profile.Data].[Funding.Role] x CROSS APPLY (select ''http://vivoweb.org/ontology/core#ResearcherRole'' TypeValue union all select ''http://vivoweb.org/ontology/core#Role'') t', oValue = 'TypeValue', OrderBy = 'TypeValue' WHERE Class = 'http://vivoweb.org/ontology/core#ResearcherRole' AND NetworkProperty IS NULL AND Property = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
 
+-- Update Datamap to fix typp in Grant Awarded By property name
+UPDATE [Ontology.].[DataMap] SET Property = 'http://profiles.catalyst.harvard.edu/ontology/prns#grantAwardedBy' where Property = 'http://profiles.catalyst.harvard.edu/ontology/prns#grandAwardedBy'
+
+-- Add missing rows into [Ontology.].[ClassProperty]
+insert into [Ontology.].[ClassProperty] ( ClassPropertyID,Class, NetworkProperty, Property, IsDetail, Limit, _TagName, _PropertyLabel, _ObjectType, MinCardinality, MaxCardinality, CustomDisplayModule, CustomEditModule, EditSecurityGroup, EditPermissionsSecurityGroup, EditExistingSecurityGroup, EditAddNewSecurityGroup, EditAddExistingSecurityGroup, EditDeleteSecurityGroup, IncludeDescription, IncludeNetwork, SearchWeight, CustomDisplay, CustomEdit, ViewSecurityGroup) 
+select max(ClassPropertyID) + 1, 'http://vivoweb.org/ontology/core#Grant', null, 'http://profiles.catalyst.harvard.edu/ontology/prns#grantAwardedBy', 0, null, 'prns:grantAwardedBy', 'grant awarded by', 1, 0, null, null, null, -40, -40, -40, -40, -40, -40, 0, 0, 0, 0, 0, -1 from [Ontology.].[ClassProperty]
+
+insert into [Ontology.].[ClassProperty] ( ClassPropertyID,Class, NetworkProperty, Property, IsDetail, Limit, _TagName, _PropertyLabel, _ObjectType, MinCardinality, MaxCardinality, CustomDisplayModule, CustomEditModule, EditSecurityGroup, EditPermissionsSecurityGroup, EditExistingSecurityGroup, EditAddNewSecurityGroup, EditAddExistingSecurityGroup, EditDeleteSecurityGroup, IncludeDescription, IncludeNetwork, SearchWeight, CustomDisplay, CustomEdit, ViewSecurityGroup) 
+select max(ClassPropertyID) + 1, 'http://vivoweb.org/ontology/core#Grant', null, 'http://profiles.catalyst.harvard.edu/ontology/prns#principalInvestigatorName', 0, null, 'prns:principalInvestigatorName', 'principal investigator name', 0, 0, null, null, null, -40, -40, -40, -40, -40, -40, 1, 0, 0, 0, 0, -1 from [Ontology.].[ClassProperty]
+
+
   -- Update all derived fields
 EXEC [Ontology.].[UpdateDerivedFields]
 EXEC [Ontology.].[UpdateCounts]
