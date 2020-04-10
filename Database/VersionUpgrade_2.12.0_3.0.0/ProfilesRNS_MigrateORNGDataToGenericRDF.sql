b6660e2ab8d62ec8815d7dc71f79d15e2ba64d26
nick_b10 <nick.b10@googlemail.com>
2020-04-10 17:09:10 -0400

    3.0.0 Upgrade Scripts

--- Database/VersionUpgrade_2.12.0_3.0.0/ProfilesRNS_MigrateORNGDataToGenericRDF.sql
+++ Database/VersionUpgrade_2.12.0_3.0.0/ProfilesRNS_MigrateORNGDataToGenericRDF.sql
@@ -15,17 +15,91 @@ insert into [RDF.Security].NodeProperty (NodeID, Property, ViewSecurityGroup)
 	select nodeID, @wNodeID, ViewSecurityGroup from [RDF.Security].NodeProperty where Property = @orngwNodeID
 update [RDF.].Triple set ViewSecurityGroup = -50 where Predicate = @orngwNodeID
 
+update [Ontology.].ClassProperty set
+	ViewSecurityGroup = -50,
+	EditSecurityGroup = -50,
+	EditPermissionsSecurityGroup = -50,
+	EditExistingSecurityGroup = -50,
+	EditAddNewSecurityGroup = -50,
+	EditAddExistingSecurityGroup = -50,
+	EditDeleteSecurityGroup = -50 
+	where property = 'http://orng.info/ontology/orng#hasYouTube'
+
+
+USE [ProfilesRNS_2_12_0]
+GO
+
+/****** Object:  Table [ORNG.].[AppData]    Script Date: 4/10/2020 4:44:03 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE #tmpVideos(
+	[NodeID] [bigint] NOT NULL,
+	[AppID] [int] NOT NULL,
+	[Keyname] [nvarchar](255) NOT NULL,
+	[Value] [nvarchar](max) NULL,
+	[CreatedDT] [datetime] NULL,
+	[UpdatedDT] [datetime] NULL
+) ON [PRIMARY]
+
+
+declare @youtubeAppID int
+SELECT @youtubeAppID = appID from [ORNG.].Apps where Name = 'Featured Videos'
+insert into #tmpVideos select * from [ORNG.].[AppData] where AppID = @youtubeAppID
+delete from #tmpVideos where [Value] = '---DATA CHUNKED BY ORNG SYSTEM---'
+
+update b set b.Value = isnull(b.Value, '') + isnull (c.value, '') + isnull(d.value, '') + isnull (e.value, '') + isnull(f.value, '') + isnull (g.value, '') + isnull(h.value, '') + isnull (i.value, '') + isnull(j.value, '') + isnull (k.value, '') from #tmpVideos b 
+	join #tmpVideos c on b.nodeid = c.nodeid and c.keyname = 'videos.1' and b.Keyname = 'videos.0'
+	left join #tmpVideos d on b.nodeid = d.nodeid and d.keyname = 'videos.2'
+	left join #tmpVideos e on b.nodeid = e.nodeid and e.keyname = 'videos.3'
+	left join #tmpVideos f on b.nodeid = f.nodeid and f.keyname = 'videos.4'
+	left join #tmpVideos g on b.nodeid = g.nodeid and g.keyname = 'videos.5'
+	left join #tmpVideos h on b.nodeid = h.nodeid and h.keyname = 'videos.6'
+	left join #tmpVideos i on b.nodeid = i.nodeid and i.keyname = 'videos.7'
+	left join #tmpVideos j on b.nodeid = j.nodeid and j.keyname = 'videos.8'
+	left join #tmpVideos k on b.nodeid = k.nodeid and k.keyname = 'videos.9'
+
+update #tmpVideos set Keyname = 'videos' where keyname = 'videos.0'
+
+delete from #tmpVideos where keyname <> 'videos'
+
+
 select @wNodeID = NodeID from [RDF.].[Node] where value in ('http://profiles.catalyst.harvard.edu/ontology/plugins#Twitter')
 select @orngwNodeID = NodeID from [RDF.].[Node] where value in ('http://orng.info/ontology/orng#hasTwitter')
 insert into [RDF.Security].NodeProperty (NodeID, Property, ViewSecurityGroup)
 	select nodeID, @wNodeID, ViewSecurityGroup from [RDF.Security].NodeProperty where Property = @orngwNodeID
 update [RDF.].Triple set ViewSecurityGroup = -50 where Predicate = @orngwNodeID
 
+update [Ontology.].ClassProperty set
+	ViewSecurityGroup = -50,
+	EditSecurityGroup = -50,
+	EditPermissionsSecurityGroup = -50,
+	EditExistingSecurityGroup = -50,
+	EditAddNewSecurityGroup = -50,
+	EditAddExistingSecurityGroup = -50,
+	EditDeleteSecurityGroup = -50 
+	where property = 'http://orng.info/ontology/orng#hasTwitter'
+
+
 select @wNodeID = NodeID from [RDF.].[Node] where value in ('http://profiles.catalyst.harvard.edu/ontology/plugins#FeaturedPresentations')
 select @orngwNodeID = NodeID from [RDF.].[Node] where value in ('http://orng.info/ontology/orng#hasSlideShare')
 insert into [RDF.Security].NodeProperty (NodeID, Property, ViewSecurityGroup)
 	select nodeID, @wNodeID, ViewSecurityGroup from [RDF.Security].NodeProperty where Property = @orngwNodeID
 update [RDF.].Triple set ViewSecurityGroup = -50 where Predicate = @orngwNodeID
+
+update [Ontology.].ClassProperty set
+	ViewSecurityGroup = -50,
+	EditSecurityGroup = -50,
+	EditPermissionsSecurityGroup = -50,
+	EditExistingSecurityGroup = -50,
+	EditAddNewSecurityGroup = -50,
+	EditAddExistingSecurityGroup = -50,
+	EditDeleteSecurityGroup = -50 
+	where property = 'http://orng.info/ontology/orng#hasSlideShare'
+
 GO
 
 DECLARE @NodeID BIGINT
@@ -52,7 +126,7 @@ DECLARE @vValue nvarchar(max)
 DECLARE @vAppID int
 SELECT @vAppID = appID from [ORNG.].Apps where Name = 'Featured Videos'
 DECLARE @curVideos CURSOR
-SET @curVideos = CURSOR FOR select NodeID, [Value] from [ORNG.].[AppData] where appID = @vAppID
+SET @curVideos = CURSOR FOR select NodeID, [Value] from #tmpVideos where appID = @vAppID
 OPEN @curVideos
 	FETCH NEXT
 	FROM @curVideos INTO @vNodeID, @vValue
@@ -65,6 +139,8 @@ OPEN @curVideos
 CLOSE @curVideos
 DEALLOCATE @curVideos
 GO
+
+drop table #tmpVideos
 
 DECLARE @sNodeID BIGINT
 DECLARE @sValue nvarchar(max)
--- Database/VersionUpgrade_2.12.0_3.0.0/ProfilesRNS_MigrateWebsitesData.sql
+++ Database/VersionUpgrade_2.12.0_3.0.0/ProfilesRNS_MigrateWebsitesData.sql
@@ -19,98 +19,42 @@
 
 declare @appID int
 select @appID = AppID from [ORNG.].[Apps] where Url = 'https://profiles.ucsf.edu/apps_2.6/Links.xml' 
-select * into #websites from [ORNG.].[AppData] where AppID = @appID
 
-update b set b.Value = isnull(b.Value, '') + isnull (c.value, '') + isnull(d.value, '') + isnull (e.value, '') + isnull(f.value, '') + isnull (g.value, '') + isnull(h.value, '') + isnull (i.value, '') + isnull(j.value, '') + isnull (k.value, '') from #websites b 
-			--on a.nodeid = b.nodeid and a.keyname = 'links' and b.keyname = 'links.0'
-	left join #websites c on b.nodeid = c.nodeid and c.keyname = 'link_1' and b.Keyname = 'link_0'
-	left join #websites d on b.nodeid = d.nodeid and d.keyname = 'link_2'
-	left join #websites e on b.nodeid = e.nodeid and e.keyname = 'link_3'
-	left join #websites f on b.nodeid = f.nodeid and f.keyname = 'link_4'
-	left join #websites g on b.nodeid = g.nodeid and g.keyname = 'link_5'
-	left join #websites h on b.nodeid = h.nodeid and h.keyname = 'link_6'
-	left join #websites i on b.nodeid = i.nodeid and i.keyname = 'link_7'
-	left join #websites j on b.nodeid = j.nodeid and j.keyname = 'link_8'
-	left join #websites k on b.nodeid = k.nodeid and k.keyname = 'link_9'
+create table #websites (
+	nodeid bigint,
+	link_name varchar(max),
+	link_url varchar(max),
+	sort_order int,
+	PersonID int,
+	GroupID int 
+)
 
-delete from #websites where keyname <> 'link_0'
-
-delete from #websites where value = '[]'
-
---select * from #websites
+insert into #websites (nodeid, link_name, link_url, sort_order)
+select NodeID, 
+	SUBSTRING([Value], CHARINDEX('"name":"', [Value]) + 8, CHARINDEX('",', [Value], CHARINDEX('"name":"', [Value]) + 8)  - CHARINDEX('"name":"', [Value]) - 8),
+	SUBSTRING([Value], CHARINDEX('"url":"', [Value]) + 7, CHARINDEX('"', [Value], CHARINDEX('"url":"', [Value]) + 7)  - CHARINDEX('"url":"', [Value]) - 7),
+	substring(keyname, 6, len(keyname) - 5) + 1 as sortorder 
+		from [ORNG.].[AppData] where AppID = @appID and Keyname <> 'links_count'
 
 
- ;WITH tmp(NodeID, AppID, DataItem, String, ordering) AS
-(
-    SELECT
-        NodeID,
-        AppID,
-        LEFT(Value, CHARINDEX('},{', Value + '},{') - 1),
-        STUFF(Value, 1, CHARINDEX('},{', Value + '},{'), ''), 0 - len(value)
-    FROM #websites
-    UNION all
-
-    SELECT
-        NodeID,
-        AppID,
-        LEFT(String, CHARINDEX('},{', String + '},{') - 1),
-        STUFF(String, 1, CHARINDEX('},{', String + '},{'), ''), 0 - len(string)
-    FROM tmp
-    WHERE
-        String > ''
-		)
-
-SELECT
-    NodeID,
-    AppID,
-    DataItem,
-	ordering
-	into #tmp2
-FROM tmp
-ORDER BY NodeID
-
--- drop table #tmp2
-
-ALTER TABLE #tmp2 ADD link_name varchar(max)
-ALTER TABLE #tmp2 ADD link_url varchar(max)
---ALTER TABLE #tmp2 ADD link_date varchar(max)
-ALTER TABLE #tmp2 ADD PersonID int 
-ALTER TABLE #tmp2 ADD GroupID int 
-
-
-update #tmp2 set link_name =
-SUBSTRING(DataItem, CHARINDEX('"name":"', DataItem) + 8, CHARINDEX('",', DataItem, CHARINDEX('"name":"', DataItem) + 8)  - CHARINDEX('"name":"', DataItem) - 8)
-where CHARINDEX('"name":"', DataItem) > 0 AND CHARINDEX('",', DataItem, CHARINDEX('"name":"', DataItem) + 8) > 0
-
-update #tmp2 set link_url =
-SUBSTRING(DataItem, CHARINDEX('"url":"', DataItem) + 7, CHARINDEX('"', DataItem, CHARINDEX('"url":"', DataItem) + 7)  - CHARINDEX('"url":"', DataItem) - 7) from #tmp2
-where CHARINDEX('"url":"', DataItem) > 0 AND CHARINDEX('"', DataItem, CHARINDEX('"url":"', DataItem) + 7) > 0
-/**
-update #tmp2 set link_date =
-SUBSTRING(DataItem, CHARINDEX('"link_date":"', DataItem) + 13, CHARINDEX('"', DataItem, CHARINDEX('"link_date":"', DataItem) + 13)  - CHARINDEX('"link_date":"', DataItem) - 13) from #tmp2
-where CHARINDEX('"link_date":"', DataItem) > 0 AND CHARINDEX('"', DataItem, CHARINDEX('"link_date":"', DataItem) + 13) > 0
-**/
-
-update t set t.PersonID = i.internalID from #tmp2 t 
+update t set t.PersonID = i.internalID from #websites t 
 join [RDF.Stage].InternalNodeMap i on 
 	t.NodeID = i.NodeID AND i.class='http://xmlns.com/foaf/0.1/Person'
 
-update t set t.GroupID = i.internalID from #tmp2 t 
+update t set t.GroupID = i.internalID from #websites t 
 join [RDF.Stage].InternalNodeMap i on 
 	t.NodeID = i.NodeID AND i.class='http://xmlns.com/foaf/0.1/Group'
 
 
 insert into [Profile.Data].[Person.Websites] (URLID, PersonID, WebPageTitle, URL,  SortOrder)
-select newID(), PersonID, link_name, link_url, Row_Number() over (partition by nodeID order by ordering) from #tmp2 where personID is not null
+select newID(), PersonID, link_name, link_url, sort_order from #websites where personID is not null
 
 insert into [Profile.Data].[Group.Websites] (URLID, GroupID, WebPageTitle, URL,  SortOrder)
-select newID(), GroupID, link_name, link_url, Row_Number() over (partition by nodeID order by ordering) from #tmp2 where GroupID is not null
+select newID(), GroupID, link_name, link_url, sort_order from #websites where GroupID is not null
 
 --select * from #websites
---select * from #tmp2
 
 drop table #websites
-drop table #tmp2
 
 /***********************
 * End of Section 1
@@ -134,6 +78,16 @@ insert into [RDF.Security].NodeProperty (NodeID, Property, ViewSecurityGroup)
 select nodeID, @wNodeID, ViewSecurityGroup from [RDF.Security].NodeProperty where Property = @orngwNodeID
 
 update [RDF.].Triple set ViewSecurityGroup = -50 where Predicate = @orngwNodeID
+
+update [Ontology.].ClassProperty set
+	ViewSecurityGroup = -50,
+	EditSecurityGroup = -50,
+	EditPermissionsSecurityGroup = -50,
+	EditExistingSecurityGroup = -50,
+	EditAddNewSecurityGroup = -50,
+	EditAddExistingSecurityGroup = -50,
+	EditDeleteSecurityGroup = -50 
+	where property = 'http://orng.info/ontology/orng#hasLinks'
 
 /***********************
 * End of Section 2
