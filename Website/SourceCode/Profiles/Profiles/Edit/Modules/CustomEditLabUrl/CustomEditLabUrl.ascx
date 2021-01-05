ba96217e7c38db36f33ee892c6933f70a240a82f
Keith Pelletier <Keith.Pelletier@umassmed.edu>
2021-01-05 17:59:16 -0500

    laburl uri

---
+++ Website/SourceCode/Profiles/Profiles/Edit/Modules/CustomEditLabUrl/CustomEditLabUrl.ascx
@@ -0,0 +1,88 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="CustomEditLabUrl.ascx.cs" Inherits="Profiles.Edit.Modules.CustomEditLabUrl.CustomEditLabUrl" %>
+<%@ Register TagName="Options" TagPrefix="security" Src="~/Edit/Modules/SecurityOptions/SecurityOptions.ascx" %>
+
+<asp:UpdatePanel ID="upnlEditSection" runat="server" UpdateMode="Conditional">
+    <ContentTemplate>
+        <asp:UpdateProgress ID="updateProgress" runat="server">
+            <ProgressTemplate>
+                <div style="position: fixed; text-align: center; height: 100px; width: 100px; top: 0; right: 0; left: 0; z-index: 9999999; opacity: 0.7;">
+                    <span style="border-width: 0px; position: fixed; padding: 50px; background-color: #FFFFFF; font-size: 25px; left: 40%; top: 40%;">
+                        <img alt="Loading..." src="../edit/images/loader.gif" /></span>
+                </div>
+            </ProgressTemplate>
+        </asp:UpdateProgress>
+        <asp:HiddenField ID="hiddenSubjectID" runat="server" />
+
+        <table id="tblEditLabUrl" width="100%">
+            <tr>
+                <td>
+                    <asp:Literal runat="server" ID="litBackLink"></asp:Literal>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div style="padding: 10px 0px;">
+                        <asp:Panel runat="server" ID="pnlSecurityOptions">
+                            <security:Options runat="server" ID="securityOptions"></security:Options>
+                        </asp:Panel>
+                        <br />
+
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table style="width: 100%;" border="1" cellspacing="2" cellpadding="4" style="border-collapse:collapse">
+                        <tr class="topRow" style="border-width: 1px; border-style: solid">
+                            <td>Lab Url</td>
+                            <td>Actions</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <asp:Panel runat="server" ID="pnlViewLabUrl" Visible="true">
+                                    <div>
+                                        <asp:Literal ID="litLabUrlText" runat="server"></asp:Literal>
+                                    </div>
+                                </asp:Panel>
+                                <asp:Panel runat="server" ID="pnlEditLabUrl" Visible="false">
+                                    <asp:TextBox runat="server"  ClientIDMode="Static" Width="100%" TextMode="Url" ID="txtLabUrlInput"></asp:TextBox>
+                                    <div>
+                                        <asp:RegularExpressionValidator 
+                                        Display="Dynamic" 
+                                        ControlToValidate="txtLabUrlInput"
+                                        ID="maxLengthValidator"
+                                        ValidationExpression="^[\s\S]{0,400}$"
+                                        runat="server"
+                                        ErrorMessage="Needs to be a webpage url"></asp:RegularExpressionValidator>
+                                    </div>
+                                </asp:Panel>
+                            </td>
+                            <td style="width: 100px" valign="top" align="center">
+                                <asp:Panel runat="server" ID="pnlSaveCancel" Visible="false">
+                                    <table class="actionbuttons">
+                                        <tr>
+                                            <td>
+                                                <asp:ImageButton ID="lnkUpdate" OnClick="btnSave_OnClick" runat="server" ImageUrl="~/Edit/Images/button_save.gif"
+                                                    CausesValidation="True" Text="Update" AlternateText="Update"></asp:ImageButton>
+                                            </td>
+                                            <td>
+                                                <asp:ImageButton ID="lnkCancel" runat="server" OnClick="btnCancelEdit_OnClick" ImageUrl="~/Edit/Images/button_cancel.gif"
+                                                    CausesValidation="False" Text="Cancel" AlternateText="Cancel"></asp:ImageButton>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </asp:Panel>
+                                <asp:Panel runat="server" ID="pnlEditButton" Visible="true">
+                                    <div class="actionbuttons">
+                                        <asp:ImageButton ID="lnkEdit" runat="server" OnClick="btnEditProperty_OnClick" ImageUrl="~/Edit/Images/icon_edit.gif" CausesValidation="False" Text="Edit" AlternateText="Edit"></asp:ImageButton>
+                                    </div>
+                                </asp:Panel>
+
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Website/SourceCode/Profiles/Profiles/Edit/Modules/CustomEditLabUrl/CustomEditLabUrl.ascx.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Xsl;
+using Profiles.Framework.Utilities;
+using Profiles.Profile.Utilities;
+using System.Globalization;
+using Profiles.Edit.Utilities;
+using System.Web.UI.HtmlControls;
+using System.Configuration;
+
+namespace Profiles.Edit.Modules.CustomEditLabUrl
+{
+    public partial class CustomEditLabUrl : BaseModule
+    {
+        Edit.Utilities.DataIO data;
+        Profiles.Profile.Utilities.DataIO propdata;
+        private Int64 SubjectID { get; set; }
+        private Int64 PredicateID { get; set; }
+        private string PredicateURI { get; set; }
+
+        private XmlDocument XMLData { get; set; }
+
+        private XmlDocument PropertyListXML { get; set; }
+        private string PropertyLabel { get; set; }
+        private string MaxCardinality { get; set; }
+        private string MinCardinality { get; set; }
+
+        private string OriginalLabUrl { get; set; }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //In terms of WebForms lifecycle, this stuff gets called second
+            // so here, we can make some decisions about whether we're loading fresh or posting back from a control
+            if (!IsPostBack)
+            {
+                Session["pnlEditLabUrl.Visible"] = null;
+            }
+            else
+            {
+                // there are probably some postback behavior we want to take here, like when Save is invoked
+            }
+
+            // either way, we almost always have to assemble the stuff that will be used on the Form
+        }
+
+        public CustomEditLabUrl() { }
+        public CustomEditLabUrl(XmlDocument pagedata, List<ModuleParams> moduleparams, XmlNamespaceManager pagenamespaces)
+            : base(pagedata, moduleparams, pagenamespaces)
+        {
+            //In terms of WebForms lifecycle, this stuff gets called first since
+            // this class gets instantiated with each request
+
+            if (Request.QueryString["subject"] != null)
+                this.SubjectID = Convert.ToInt64(Request.QueryString["subject"]);
+            else if (base.GetRawQueryStringItem("subject") != null)
+                this.SubjectID = Convert.ToInt64(base.GetRawQueryStringItem("subject"));
+            else
+                Response.Redirect("~/search");
+
+            //appears this isn't really used
+            SessionManagement sm = new SessionManagement();
+
+
+            //initialize data access in the edit namespace
+            data = new Profiles.Edit.Utilities.DataIO();
+
+            //this is plucked off the querystring
+            string predicateuri = Request.QueryString["predicateuri"].Replace("!", "#");
+            this.PredicateURI = predicateuri;
+
+            this.initializeForm();
+
+            securityOptions.Subject = this.SubjectID;
+            securityOptions.PredicateURI = predicateuri;
+            securityOptions.PrivacyCode = Convert.ToInt32(this.PropertyListXML.SelectSingleNode("PropertyList/PropertyGroup/Property/@ViewSecurityGroup").Value);
+            securityOptions.SecurityGroups = new XmlDataDocument();
+            securityOptions.SecurityGroups.LoadXml(base.PresentationXML.DocumentElement.LastChild.OuterXml);
+        }
+
+        protected void btnEditProperty_OnClick(object sender, EventArgs e)
+        {
+            if (Session["pnlEditLabUrl.Visible"] != null)
+            {
+                Session["pnlEditLabUrl.Visible"] = null;
+                pnlEditLabUrl.Visible = false;
+                pnlViewLabUrl.Visible = true;
+                pnlEditButton.Visible = true;
+                pnlSaveCancel.Visible = false;
+            }
+            else
+            {
+                pnlEditLabUrl.Visible = true;
+                pnlViewLabUrl.Visible = false;
+                pnlEditButton.Visible = false;
+                pnlSaveCancel.Visible = true;
+                Session["pnlEditLabUrl.Visible"] = true;
+            }
+            upnlEditSection.Update();
+        }
+
+        protected void btnSave_OnClick(object sender, EventArgs e)
+        {
+            // only really need to do this when saving
+            this.PredicateID = data.GetStoreNode(this.PredicateURI);
+            var hasErrors = doSaveSummery();
+            if (!hasErrors)
+            {
+                // we need to refresh the display now that we've saved since above, it was using the values before they got into the database
+                // PropertyListXML still has the old values
+                this.initializeForm(true);
+            }
+            // update panel visibility
+            updatePanels();
+        }
+
+        protected void btnCancelEdit_OnClick(object sender, EventArgs e)
+        {
+            // update panel visibility
+            updatePanels();
+        }
+
+        private void initializeForm(bool refresh = false)
+        {
+            // data access in the Profiles namespace 
+            propdata = new Profiles.Profile.Utilities.DataIO();
+
+            if (refresh)
+            {
+                base.GetSubjectProfile();
+                this.PropertyListXML = propdata.GetPropertyList(this.BaseData, base.PresentationXML, this.PredicateURI, false, true, false);
+            }
+
+            // the propertylistxml has some information that is useful for deciding what property we're deling with
+            // so it's good to just it early here
+            this.PropertyListXML = propdata.GetPropertyList(this.BaseData, base.PresentationXML, this.PredicateURI, false, true, false);
+
+            PropertyLabel = this.PropertyListXML.SelectSingleNode("PropertyList/PropertyGroup/Property/@Label").Value;
+
+            litBackLink.Text = "<a href='" + Root.Domain + "/edit/" + this.SubjectID + "'>Edit Menu</a> &gt; <b>" + PropertyListXML.SelectSingleNode("PropertyList/PropertyGroup/Property/@Label").Value + "</b>";
+
+            XmlNode connectionNode = this.PropertyListXML.SelectSingleNode("PropertyList/PropertyGroup/Property/Network/Connection");
+            string propertyConnectionValue = connectionNode.InnerText;
+            OriginalLabUrl = connectionNode.InnerText;
+
+            txtLabUrlInput.Text = propertyConnectionValue;
+            litLabUrlText.Text = propertyConnectionValue;
+        }
+
+        private void updatePanels()
+        {
+            pnlEditLabUrl.Visible = false;
+            pnlViewLabUrl.Visible = true;
+            pnlEditButton.Visible = true;
+            pnlSaveCancel.Visible = false;
+            Session["pnlEditLabUrl.Visible"] = null;
+            upnlEditSection.Update();
+        }
+
+        private bool doSaveSummery()
+        {
+            return data.UpdateLiteral(this.SubjectID, this.PredicateID, data.GetStoreNode(this.OriginalLabUrl), data.GetStoreNode(txtLabUrlInput.Text.Replace("\n", "").Trim()), this.PropertyListXML);
+
+        }
+    }
+}
